ff5648980988e8412dcd44afec6db94e6a12a743
Pavel Kotek <kotekp21@spstrutnov.cz>
2024-02-06 16:10:04 +0100

    Je potřeba začít přemýšlet nad tím jak stránka bude strukturovaná

--- src/app/routers/allRouter.js
+++ src/app/routers/allRouter.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+router.use('/lobby', require('./lobbyRouter'));
+
 router.get('*', (req, res) => {
 	res.status(404);
 	res.send('Stránka neexistuje (404)');
---
+++ src/app/routers/lobbyRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Odkazy na controller
+
+module.exports = router;
