b5dcd4ec3515c2a5bcb022b3b98bf02d7828de07
Pavel Kotek <kotekp21@spstrutnov.cz>
2024-02-11 09:47:57 +0100

    Hádám, že nějaké to rozpoložení?

--- src/app/routers/allRouter.js
+++ src/app/routers/allRouter.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 router.use('/lobby', require('./lobbyRouter'));
+router.use('/account', require('./accountRouter'));
+router.use('/game', require('./gameRouter'));
 
 router.get('*', (req, res) => {
 	res.status(404);
