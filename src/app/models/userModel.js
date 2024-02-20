adae76b495bd6c8bef6d868e2512734b15bc22a2
Pavel Kotek <kotekp21@spstrutnov.cz>
2024-02-20 16:23:18 +0100

    Ono to nějak půjde

--- src/app/models/userModel.js
+++ src/app/models/userModel.js
@@ -1,3 +1,19 @@
 const jsondb = require('simple-json-db');
 const db = new jsondb('./data/users.json');
 
+if(!db.has('next_id')) {
+    db.set('next_id', 1);
+}
+
+exports.pridatUzivatele = (jmeno, heslo) => {
+    let id = db.get('next_id');
+
+    db.set(id, {
+        'jmeno': jmeno,
+        'heslo': heslo
+    });
+
+    id += 1;
+
+    db.set('next_id', id);
+}
