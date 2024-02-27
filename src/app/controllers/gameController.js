3769697133a4d5e019540aed4f182d507e5ed77f
Pavel Kotek <kotekp21@spstrutnov.cz>
2024-02-27 17:10:11 +0100

    Rovnání karet!

--- src/app/controllers/gameController.js
+++ src/app/controllers/gameController.js
@@ -9,3 +9,13 @@ exports.dealCardsVoleny = (req, res) => {
     tableModel.dealCardsVoleny(1);
     res.redirect('/lobby/index');
 }
+
+exports.sortCards = (req, res) => {
+    tableModel.sortCards(1, "Josef", true);
+    res.redirect('/lobby/index');
+}
+
+exports.recollectCards = (req, res) => {
+    tableModel.recollectCards(1);
+    res.redirect('/lobby/index');
+}
