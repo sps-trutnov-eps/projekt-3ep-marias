5c058ca35bb26414997a9351274673f26a92d675
Kačer Šimon <kacers21@spstrutnov.cz>
2024-02-27 15:39:09 +0100

    Vytvoření stránek na volený a lízaný mariáš. A úspěšné přesměrování.

--- src/app/routers/lobbyRouter.js
+++ src/app/routers/lobbyRouter.js
@@ -7,5 +7,7 @@ const lobbyController = require('../controllers/lobbyController');
 // Odkazy na controller
 router.get('/index', lobbyController.index);
 router.get('/addTable', lobbyController.addTable);
+router.get('/lizany', lobbyController.lizany);
+router.get('/voleny', lobbyController.voleny);
 
 module.exports = router;
