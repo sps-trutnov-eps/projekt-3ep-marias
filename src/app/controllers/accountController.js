c1078e7775fec53e4b7bf22d9864c724be3420f3
Kačer Šimon <kacers21@spstrutnov.cz>
2024-03-17 17:35:14 +0100

    Vytvořená stránka na přihlášení a přesměrováno přes router.

--- src/app/controllers/accountController.js
+++ src/app/controllers/accountController.js
@@ -1,3 +1,9 @@
+const userModel = require("../models/userModel");
+
 exports.index = (req, res) => {
     res.render('account/index');
+}
+
+exports.prihlasit = (req, res) => {
+    res.render('account/prihlasit');
 }
