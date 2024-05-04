8da7dc30b0e9daea0be9a7ed50f4d3f24d0dc629
Kačer Šimon <kacers21@spstrutnov.cz>
2024-05-04 14:23:28 +0200

    Vykreslování stolů v lobby. Potřeba pořešit připojování do nich.
    Jeden takový problém, nezobrazují se nově vytvořené stoly. Nevím jestli je chyba v mém JS, nebo na straně serveru.

--- src/www/js/lobby.js
+++ src/www/js/lobby.js
@@ -1,3 +1,6 @@
+let loadedTablesVoleny = [];
+let loadedTablesLizany = [];
+
 function getTablesVoleny() {
     fetch('stolyVoleny', {
         method: 'POST',
@@ -6,14 +9,65 @@ function getTablesVoleny() {
         },
     })      
     .then(response => response.json())
-    .then(data => {
-        // data je teď velký list plný všech her co máš v databázi
+    .then(data => {        
         console.log(data);
+        const newTables = data.filter(table => !loadedTablesVoleny.some(loadedTable => loadedTable.name === table.name));
+
+        loadedTablesVoleny.push(...newTables);
+
+        const tableList = document.querySelector('#tableDivVoleny ul');
+
+        newTables.forEach(table => {
+            const tableButton = document.createElement('button');
+            tableButton.textContent = 'Připojit do hry';
+            tableButton.classList.add('btn', 'btn-success', 'mx-3');
+            const listItem = document.createElement('li');
+        
+            const { name, password } = table;
+        
+            const nameElement = document.createElement('span');
+            nameElement.textContent = name;
+            nameElement.classList.add('px-5', 'fw-bold');
+        
+            const passwordElement = document.createElement('span');
+            passwordElement.textContent = `Heslo: `;
+        
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.checked = password.length > 0;
+            checkbox.disabled = true;
+            checkbox.classList.add('checkbox-custom');
+        
+            passwordElement.appendChild(checkbox);
+        
+            listItem.appendChild(nameElement);
+            listItem.appendChild(passwordElement);
+            listItem.appendChild(tableButton); 
+            listItem.classList.add('mb-2');
+        
+            tableButton.addEventListener('click', () => {
+                if (password.length > 0) {
+                    const passwordInput = prompt('Zadejte heslo pro připojení k hře:');
+                    if (passwordInput === password) {
+                        console.log(`Heslo pro stůl ${table.name} bylo správné.`);
+                    } else {
+                        console.log(`Zadali jste nesprávné heslo pro stůl ${table.name}.`);
+                    }
+                } else {
+                    console.log(`Připojuji se k hře na stolu ${table.name}.`);
+                }
+            });
+        
+            tableList.appendChild(listItem);
+        });
+
     });
 }
 
+setInterval(getTablesVoleny, 3000);
+
 // function getTablesLizany() {
-//     fetch('lobby/stolyLizany', {
+//     fetch('stolyLizany', {
 //         method: 'POST',
 //         headers: {
 //             'Content-Type': 'application/json',
@@ -22,5 +76,57 @@ function getTablesVoleny() {
 //     .then(response => response.json)
 //     .then(data => {
 //         console.log(JSON.parse(data));
-//     });
-// }
+//         const newTables = data.filter(table => !loadedTablesLizany.some(loadedTable => loadedTable.name === table.name));
+
+//         loadedTablesLizany.push(...newTables);
+
+//         const tableList = document.querySelector('#tableDivLizany ul');
+
+//         newTables.forEach(table => {
+//             const tableButton = document.createElement('button');
+//             tableButton.textContent = 'Připojit do hry';
+//             tableButton.classList.add('btn', 'btn-success', 'mx-3');
+//             const listItem = document.createElement('li');
+
+//             const { name, password } = table;
+
+//             const nameElement = document.createElement('span');
+//             nameElement.textContent = name;
+//             nameElement.classList.add('px-5', 'fw-bold');
+
+//             const passwordElement = document.createElement('span');
+//             passwordElement.textContent = `Heslo: `;
+
+//             const checkbox = document.createElement('input');
+//             checkbox.type = 'checkbox';
+//             checkbox.checked = password.length > 0;
+//             checkbox.disabled = true;
+//             checkbox.classList.add('checkbox-custom');
+
+//             passwordElement.appendChild(checkbox);
+
+//             listItem.appendChild(nameElement);
+//             listItem.appendChild(passwordElement);
+//             listItem.appendChild(tableButton); 
+//             listItem.classList.add('mb-2');
+
+//             tableButton.addEventListener('click', () => {
+//                 if (password.length > 0) {
+//                     const passwordInput = prompt('Zadejte heslo pro připojení k hře:');
+//                     if (passwordInput === password) {
+//                         console.log(`Heslo pro stůl ${table.name} bylo správné.`);
+//                     } else {
+//                         console.log(`Zadali jste nesprávné heslo pro stůl ${table.name}.`);
+//                     }
+//                 } else {
+//                     console.log(`Připojuji se k hře na stolu ${table.name}.`);
+//                 }
+//             });
+
+//             tableList.appendChild(listItem);
+//         });
+
+//      });
+//  }
+
+//  setInterval(getTablesLizany, 3000);
